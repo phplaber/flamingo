0c6e225439aff5602c2ecb48d4b87fd3335db648
yns0ng <phplaber@gmail.com>
2024-05-09 22:17:54 +0800

    丢弃不需要的请求对象

--- utils.go
+++ utils.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	b64 "encoding/base64"
+	"errors"
+	"net/url"
+	"strings"
 	"sync"
 )
 
 var mu sync.Mutex
+
+var extWlist = []string{".php", ".asp", ".jsp", ".html", ".htm"}
 
 type request struct {
 	method  string
@@ -13,6 +18,18 @@ type request struct {
 	headers map[string]interface{}
 	data    string //base64 编码
 	source  string
+}
+
+func getFileExtFromUrl(rawUrl string) (string, error) {
+	u, err := url.Parse(rawUrl)
+	if err != nil {
+		return "", err
+	}
+	pos := strings.LastIndex(u.Path, ".")
+	if pos == -1 {
+		return "", errors.New("couldn't find a period to indicate a file extension")
+	}
+	return u.Path[pos:len(u.Path)], nil
 }
 
 func geneRequest(method string, url string, headers map[string]interface{}, data string, source string) *request {
@@ -26,6 +43,29 @@ func geneRequest(method string, url string, headers map[string]interface{}, data
 }
 
 func saveRequest(reqs *[]request, req *request) {
+	url := strings.ToLower(req.url)
+
+	// 过滤非 HTTP 请求
+	if !strings.HasPrefix(url, "http") {
+		return
+	}
+
+	// 只保留特定 URL，如：后缀 .php，.asp，.jsp，.html 和 .htm 等
+	urlExt, _ := getFileExtFromUrl(url)
+	if urlExt != "" {
+		var skip = true
+		for _, ext := range extWlist {
+			if urlExt == ext {
+				skip = false
+				break
+			}
+		}
+
+		if skip {
+			return
+		}
+	}
+
 	mu.Lock()
 	*reqs = append(*reqs, *req)
 	mu.Unlock()
